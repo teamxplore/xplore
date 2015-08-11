89b73e40f5a3845e08c46ad9dc7e46127167acba
Cliff Saporta Cheng <ccfcheng@gmail.com>
2015-08-11 12:53:07 -0700

    Can now respond to get requests from client to search on Yelp
    TODO: set up post request listener for a specific query from client

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var yelp = require('./yelpRequest');
+var utils = require('./utils');
 var app = express();
 
 app.use(express.static(__dirname + "/../public"));
@@ -18,20 +19,30 @@ var searchParams = {
   location: 'Paris, France'
 };
 
-var businessParams = "musée-d-orsay-paris-3";
+// app.get('/url', function(req, res) {
+//   var url = 'http://s3-media4.fl.yelpcdn.com/bphoto/8ZcN-JDxU6ixJFsR1UjC6w/ms.jpg';
+//   var data = utils.getLargeImg(url);
+//   res.send(data);
+// });
+
+// var businessParams = "musée-d-orsay-paris-3";
 
 app.get('/search', function(req, res) {
   yelp.explore(searchParams, function(data) {
-    res.send(data);
+    var results = [];
+    data.businesses.forEach(function(listing) {
+      results.push(utils.makeExploreEntry(listing));
+    });
+    res.send(results);
   });
 });
 
-app.get('/business', function(req, res) {
-  yelp.business(businessParams, function(error, data) {
-    console.log(error);
-    res.send(data);
-  });
-});
+// app.get('/business', function(req, res) {
+//   yelp.business(businessParams, function(error, data) {
+//     console.log(error);
+//     res.send(data);
+//   });
+// });
 
 // yelp.search({term: "ramen", location: "Sacramento, CA"}, function(error, data) {
 //   console.log(error);
---
+++ server/utils.js
@@ -0,0 +1,32 @@
+var url = require('url');
+var path = require('path');
+
+module.exports = {
+
+  // takes in small /ms.jpg path and changes to url of original size at /o.jpg
+  getLargeImg: function(address) {
+    var oldUrl = url.parse(address);
+    var oldPath = path.parse(oldUrl.pathname);
+    // set new base in oldPath to be o.jpg
+    oldPath.base = 'o.jpg';
+    // console.log(oldPath);
+    // set new path in oldUrl
+    oldUrl.pathname = path.format(oldPath);
+    // return new url
+    console.log(oldUrl);
+    return url.format(oldUrl);
+  },
+
+  // this callback takes a single Yelp business's JSON data and parses out what we want
+  makeExploreEntry: function(listing) {
+    return {
+      name: listing.name,
+      imgUrl: this.getLargeImg(listing.image_url),
+      address: listing.location.display_address, // this is an array where each element is an address line
+      description: listing.snippet_text,
+      distance: listing.distance,
+      reviews: listing.review_count,
+      ratingUrl: listing.rating_img_url
+    };
+  }
+};
