35422e8d3302f9dd9ad5ceb1378ab19ff1d3ff0b
Wesley Tsai <wezley.tsai@gmail.com>
2015-08-11 14:29:59 -0700

    (feat) Route uber API calls through node server

--- client/app/services/uber.js
+++ client/app/services/uber.js
@@ -1,60 +1,50 @@
 angular.module('uberxplore.services', [])
 .factory('Uber', function($http) {
-  var url = 'https://sandbox-api.uber.com/';
-  var client_id = 'sGV-9D8qBynnWoxef1Ypx2CdlHR7o1ur';
-  var server_token = 'kRsZskOJWvh2zRsLAQD33RxjJWoqGf65z3K0JI8z';
-
-  var endpoints = {
-    products: 'v1/products',
-    priceEstimates: 'v1/estimates/price',
-    timeEstimates: 'v1/estimates/time'
-  };
-
   var login = function() {
 
   };
 
-  var getProducts = function() {
+  var getProducts = function(latitude, longitude) {
     var params = {
-      latitude: 37.775818, // TODO: fill in with user current location
-      longitude: -122.418028 // TODO: fill in with user current location
+      // --- uncomment these when ready
+      // latitude: latitude,
+      // longitude: longitude
+      latitude: 37.775818,
+      longitude: -122.418028
     };
-    return $http.get(url+endpoints.products, {
-      headers: {
-        Authorization: 'Token '+server_token
-      },
-      params: params
-    });
+
+    return $http.get('/uber/products', {params: params});
   };
 
-  var getPriceEstimates = function() {
+  var getPriceEstimates = function(startLatitude, startLongitude, endLatitude, endLongitude) {
     var params = {
-      start_latitude: 37.334381, // TODO: fill in with user current location
-      start_longitude: -121.89432, // TODO: fill in with user current location
-      end_latitude: 37.77703, // TODO: fill in with user destination
-      end_longitude: -122.419571, // TODO: fill in with user destination
+      // --- uncomment these when ready
+      // start_latitude: startLatitude,
+      // start_longitude: startLongitude,
+      // end_latitude: endLatitude,
+      // end_longitude: endLongitude
+      start_latitude: 37.334381,
+      start_longitude: -121.89432,
+      end_latitude: 37.77703,
+      end_longitude: -122.419571,
     };
-    return $http.get(url+endpoints.priceEstimates, {
-      headers: {
-        Authorization: 'Token '+server_token
-      },
-      params: params
-    });
+
+    return $http.get('/uber/price', {params: params});
   };
 
-  var getTimeEstimates = function() {
+  var getTimeEstimates = function(startLatitude, startLongitude, endLatitude, endLongitude) {
     var params = {
-      start_latitude: 37.334381, // TODO: fill in with user current location
-      start_longitude: -121.89432, // TODO: fill in with user current location
-      end_latitude: 37.77703, // TODO: fill in with user destination
-      end_longitude: -122.419571, // TODO: fill in with user destination
+      // --- uncomment these when ready
+      // start_latitude: startLatitude,
+      // start_longitude: startLongitude,
+      // end_latitude: endLatitude,
+      // end_longitude: endLongitude
+      start_latitude: 37.334381,
+      start_longitude: -121.89432,
+      end_latitude: 37.77703,
+      end_longitude: -122.419571,
     };
-    return $http.get(url+endpoints.timeEstimates, {
-      headers: {
-        Authorization: 'Token '+server_token
-      },
-      params: params
-    });
+    return $http.get('/uber/time', {params: params});
   };
 
   return {
