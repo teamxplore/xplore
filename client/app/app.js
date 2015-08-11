34989d83623a616d3f9a1410660681c4940daf98
Kamerynn Harrah <kamharrah8@gmail.com>
2015-08-11 13:39:51 -0400

    (feat) Impl app/folder structure, ui-routing, and index

---
+++ client/app/app.js
@@ -0,0 +1,28 @@
+angular.module('uberxplore', [
+  'uberxplore.itineraryList',
+  'uberxplore.locations',
+  'uberxplore.services',
+  'ui.router'
+])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  // $urlRouterProvider.otherwise();
+
+  // Uncomment the states when implementing them
+  $stateProvider
+    .state('explore', {
+      // url: '/explore',
+      // templateUrl:'app/explore/explore.html',
+      // controller: 'ExploreController'
+    })
+    .state('list', {
+      url: '/list',
+      templateUrl:'app/itinerary/list/itineraryList.html',
+      controller: 'ItineraryListController'
+    })
+    .state('map', {
+      // url: '/map',
+      // templateUrl:'app/itinerary/list/itineraryMap.html',
+      // controller: 'ItineraryMapController'
+    })
+});
